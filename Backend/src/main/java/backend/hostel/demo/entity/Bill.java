dd4623b008c7714f78819a323cc053e30d640c8c
Lam Thi Hong Nam <lamthihongnam0407@gmail.com>
2023-09-16 23:03:43 +0700

    Tạo các tập tin bill.java, billdetail.java,payment.java,paymentmethod,user.java

---
+++ Backend/src/main/java/backend/hostel/demo/entity/Bill.java
@@ -0,0 +1,40 @@
+package backend.hostel.demo.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Table (name = "bill")
+public class Bill {
+	@Id
+	@Column (name = "bill_id")
+	private String billId;
+	@Column (name = "room_id")
+	private String roomId;
+	@Column (name = "username")
+	private String username;
+	@Column (name = "createddate")
+	private Date createdDate;
+	@Column (name = "paiddate")
+	private Date paidDate;
+	@Column (name = "totalamount")
+	private float totalAmount;
+	@Column (name = "month")
+	private int month;
+	@Column (name = "year")
+	private int year;
+	
+
+}
---
+++ Backend/src/main/java/backend/hostel/demo/entity/Payment.java
@@ -0,0 +1,42 @@
+package backend.hostel.demo.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table (name ="payment")
+public class Payment {
+	@Id
+	@Column (name = "payment_id")
+	private String paymentId;
+	
+	@Column (name = "method_id")
+	private String methodId;
+	
+	@Column (name = "payment_date")
+	private String paymentDate;
+	
+	@Column (name = "username")
+	private String username;
+	
+	@Column(name = "amount")
+	private String amount;
+	
+	@Column (name = "description")
+	private String description;
+	@Column(name = "created_date")
+	private Date createdDate;
+	
+
+}
