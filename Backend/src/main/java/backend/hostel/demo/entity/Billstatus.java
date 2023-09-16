ded8f3eb071e71f0957cbe45b367d18be985bd59
Le Kim Ngan <kimngann.2522@gmail.com>
2023-09-16 23:08:18 +0700

    Tạo các tập tin Billstatus, Contract, Index, Receipt, Room, Roomstatus, Service, ServiceOfRoom, StatusContract, Tenant, TenantStatus

---
+++ Backend/src/main/java/backend/hostel/demo/entity/Billstatus.java
@@ -0,0 +1,27 @@
+package backend.hostel.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bill_status")
+public class Billstatus {
+	@Id
+	@Column(name = "status_id")
+	private String statusId;
+	
+	@Id
+	@Column(name = "bill_id")
+	private String billId;
+
+}
---
+++ Backend/src/main/java/backend/hostel/demo/entity/Contract.java
@@ -0,0 +1,46 @@
+package backend.hostel.demo.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contract")
+public class Contract {
+	@Id
+	@Column(name = "contract_id")
+	private String contractId;
+	
+	@Column(name = "tenant_id")
+	private String tenantId;
+	
+	@Column(name = "username")
+	private String userName;
+	
+	@Column(name = "start_date")
+	private Date startDate;
+	
+	@Column(name = "end_date")
+	private Date endDate;
+	
+	@Column(name = "create_date")
+	private Date createDate;
+	
+	@Column(name = "note")
+	private String note;
+
+	
+
+
+}
