276f25de3aab46f068d818024a6b446a3930d147
Le Kim Ngan <kimngann.2522@gmail.com>
2023-09-18 17:43:00 +0700

    Tạo các tệp tin BillDetailDto, BillDto, BillStatusDto, ContractDto, IndexDto, PaymentDto, PaymentMethodDto, ReceiptDto, RoomDto, RoomStatusDto, ServiceDto, ServiceOfRoomDto, StatusContractDto, TenantDto, TenantStatusDto, UserDto

---
+++ Backend/src/main/java/backend/hostel/demo/dto/BillStatusDto.java
@@ -0,0 +1,17 @@
+package backend.hostel.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillStatusDto {
+	private String statusId;
+	
+	private String billId;
+
+}
