634b087f070ce4ee316c4e1983eaec954733236c
Lam Thi Hong Nam <lamthihongnam0407@gmail.com>
2023-09-20 12:10:47 +0700

    tạo tệp tin BillRepo, BillDetailRepo, PaymentRepo, PaymentMethodRepo, UserRepo

---
+++ Backend/src/main/java/backend/hostel/demo/repository/BillRepo.java
@@ -0,0 +1,9 @@
+package backend.hostel.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import backend.hostel.demo.entity.Bill;
+
+public interface BillRepo extends CrudRepository<Bill, String> {
+
+}
