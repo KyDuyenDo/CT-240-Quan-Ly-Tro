cbd01a5a815907c9fae88d7598a7f5892c487c29
Nguyen Ngoc Truyen <nntruyen027@gmail.com>
2023-09-25 14:20:07 +0700

    Cập nhật các lới liên quan đến tenant

--- Backend/src/main/java/backend/hostel/demo/service/implement/TenantImpl.java
+++ Backend/src/main/java/backend/hostel/demo/service/implement/TenantImpl.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import backend.hostel.demo.dto.TenantDto;
+import backend.hostel.demo.entity.Tenant;
+import backend.hostel.demo.mapper.TenantMapper;
 import backend.hostel.demo.repository.TenantRepo;
+import backend.hostel.demo.repository.TenantStatusRepo;
 import backend.hostel.demo.service.TenantService;
 
 @Component
@@ -13,59 +16,61 @@ public class TenantImpl implements TenantService {
 	@Autowired
 	private TenantRepo tenantRepo;
 	
+	@Autowired
+	private TenantStatusRepo tenantStatusRepo;
+	
 
 	@Override
 	public Iterable<TenantDto> getTenants() {
-		// TODO Auto-generated method stub
-		return null;
+		return TenantMapper.toDtoList(tenantRepo.findAll());
 	}
 
 	@Override
 	public Iterable<TenantDto> getTenantByName(String fullname) {
-		// TODO Auto-generated method stub
-		return null;
+		return TenantMapper.toDtoList(tenantRepo.findAllByName(fullname));
 	}
 
 	@Override
 	public Iterable<TenantDto> getTenantByRoom(String roomId) {
-		// TODO Auto-generated method stub
-		return null;
+		return TenantMapper.toDtoList(tenantRepo.findAllByRoom(roomId));
 	}
 
 	@Override
 	public Iterable<TenantDto> getTenantByStatus(String statusId) {
-		// TODO Auto-generated method stub
-		return null;
+		return TenantMapper.toDtoList(tenantStatusRepo.findTenantByStatus(statusId));
 	}
 
 	@Override
 	public TenantDto getTenantById(String tenantId) {
-		// TODO Auto-generated method stub
-		return null;
+		return TenantMapper.toDto(tenantRepo.findById(tenantId).get());
 	}
 
 	@Override
-	public TenantDto createTenant(TenantDto newTenant) {
-		// TODO Auto-generated method stub
-		return null;
+	public TenantDto createTenant(TenantDto tenantDto) {
+		Tenant tenant = new Tenant(tenantDto.getTenantId(), tenantDto.getRoomId(), tenantDto.getTenantName(), tenantDto.getPhoneNumber()
+				,tenantDto.getAddress(), tenantDto.getDateOfBirth(), tenantDto.getWork());
+		
+		return TenantMapper.toDto(tenantRepo.save(tenant));
+				
 	}
 
 	@Override
-	public TenantDto updateTenant(TenantDto tenant) {
-		// TODO Auto-generated method stub
-		return null;
+	public TenantDto updateTenant(TenantDto tenantDto) {
+		Tenant tenant = tenantRepo.findById(tenantDto.getTenantId()).orElse(null);
+		
+		if(tenant == null)
+			return null;
+		else {
+			tenant = TenantMapper.toEntity(tenantDto);
+			return TenantMapper.toDto(tenantRepo.save(tenant));
+		}
 	}
 
 	@Override
 	public TenantDto deleteTenant(String tenantId) {
-		// TODO Auto-generated method stub
-		return null;
+		tenantRepo.deleteById(tenantId);
+		return TenantMapper.toDto(tenantRepo.findById(tenantId).get());
 	}
 
-	@Override
-	public TenantDto moveTenant(String tenantId, String roomId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
